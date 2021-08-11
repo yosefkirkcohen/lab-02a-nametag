ab683f4b87bd9ade5b4a50f202b19bcdcad1dfb8
yosefkirkcohen <ycohen45@yahoo.com>
2021-08-11 16:33:42 -0700

    color change

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ const yourName = document.getElementById('your-name');
 myButton.addEventListener('click', () => {
     yourName.textContent = myInput.value
 })
+
+const colorButton = document.getElementById('color-button');
+
+const colorTop = document.getElementsByClassName('top')
+console.log(colorTop)
+colorButton.addEventListener('click', () => {
+    colorTop[0].style.backgroundColor = "blue"
+})
